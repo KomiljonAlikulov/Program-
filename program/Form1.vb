d09315b2fdce12f63b962f6988520e162a82ba2c
Owner <Owner@DESKTOP-8LMQON8>
2020-02-05 09:18:55 -0800

    Add project files.

---
+++ program/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Integer
+        a = Val(TextBox1.Text)
+        ListBox1.Items.Clear()
+        For i = 1 To 10
+            ListBox1.Items.Add(i & " x " & a & " = " & a * i)
+        Next
+
+    End Sub
+End Class
